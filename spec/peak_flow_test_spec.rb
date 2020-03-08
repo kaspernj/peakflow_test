0dc280d0c7c80b0f5a26760e71da67a9c426d9c2
kaspernj <kaspernj@gmail.com>
2020-03-08 09:40:07 +0000

    Test that everything works

--- spec/peak_flow_test_spec.rb
+++ spec/peak_flow_test_spec.rb
@@ -1,7 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe 'PeakFlowTest' do
-  it 'fails' do
-    fail 'hey buddy, you should probably rename this file and start specing for real'
+  it 'connects to the postgres database' do
+    require "pg"
+
+    rows = []
+
+    conn = PG.connect(host: "postgres", dbname: "database", user: "user", password: "password")
+    conn.exec("SELECT 1 AS count") do |result|
+      result.each do |row|
+        rows << row.to_h
+      end
+    end
+
+    expect(rows).to eq [{count: 1}]
   end
 end

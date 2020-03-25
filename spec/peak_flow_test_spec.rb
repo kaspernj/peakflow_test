36a1a7c396c1c39ba8eac6774dd176138c2d6876
kaspernj <kaspernj@gmail.com>
2020-03-25 10:09:59 +0000

    Expect it with Ruby

--- spec/peak_flow_test_spec.rb
+++ spec/peak_flow_test_spec.rb
@@ -4,6 +4,6 @@ describe 'PeakFlowTest' do
   it 'injects the right env vars' do
     expect(ENV.fetch("FIRST_NAME")).to eq "Kasper"
     expect(ENV.fetch("LAST_NAME")).to eq "Johansen"
-    expect(ENV.fetch("RUBY_VERSION")).to eq "2.5.7"
+    expect(ENV.fetch("RUBY_VERSION")).to eq "ruby-2.5.7"
   end
 end

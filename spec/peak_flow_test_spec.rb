37a641b523aa726f8e6aa96bdd4341dd302bba13
kaspernj <kaspernj@gmail.com>
2020-03-25 08:26:41 +0000

    Added env vars test

--- spec/peak_flow_test_spec.rb
+++ spec/peak_flow_test_spec.rb
@@ -1,7 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe 'PeakFlowTest' do
-  it 'fails' do
-    fail 'hey buddy, you should probably rename this file and start specing for real'
+  it 'injects the right env vars' do
+    expect(ENV.fetch("FIRST_NAME")).to eq "Kasper"
+    expect(ENV.fetch("LAST_NAME")).to eq "Johansen"
   end
 end

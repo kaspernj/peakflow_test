ed1e0a3126849b4832f347692a2804a4fa9faa22
kaspernj <k@spernj.org>
2024-11-16 11:45:37 +0000

    Fixed expect

--- spec/peak_flow_test_spec.rb
+++ spec/peak_flow_test_spec.rb
@@ -4,6 +4,6 @@ describe "PeakFlowTest" do
   it "injects the right env vars" do
     expect(ENV.fetch("FIRST_NAME")).to eq "Kasper"
     expect(ENV.fetch("LAST_NAME")).to eq "Johansen"
-    expect(ENV.fetch("RUBY_VERSION")).to eq "ruby-2.7.5"
+    expect(ENV.fetch("RUBY_VERSION")).to eq "ruby-3.3.6"
   end
 end
